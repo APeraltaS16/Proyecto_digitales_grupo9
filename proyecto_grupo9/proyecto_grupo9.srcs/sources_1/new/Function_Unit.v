28c129bf556c85ab143b53cdc79fb3ca5a924272
APeraltaS16 <aps@uc.cl>
2022-11-30 22:05:03 -0300

    update inicial

---
+++ proyecto_grupo9/proyecto_grupo9.srcs/sources_1/new/Function_Unit.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 30.11.2022 18:37:44
+// Design Name: 
+// Module Name: Function_Unit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Function_Unit(
+
+    );
+endmodule
---
+++ proyecto_grupo9/proyecto_grupo9.srcs/sources_1/new/Register_File.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 30.11.2022 18:37:44
+// Design Name: 
+// Module Name: Register_File
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Register_File(
+
+    );
+endmodule
